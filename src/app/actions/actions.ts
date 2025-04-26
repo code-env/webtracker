65019eac10da2ba0b6034e6a71f4ba3456db1583
Avik-creator <avikm744@gmail.com>
2025-04-26 20:28:38 +0530

    Added Changes.

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -2,49 +2,41 @@
 
 import { getDomainAnalytics, getDomainProject, getProjects } from "./db_calls";
 
-import { unstable_cache as cache } from "next/cache";
-
 export const getAllProjects = async (id: string | undefined) => {
-    if (!id) {
-      return null;
-    }
-    try {
-      const res = await cache(
-        async () => {
-          const projects = await getProjects(id);
-          return projects || [];
-        },
-        ["all-projects", id],
-        { tags: ["projects"] },
-      )();
-      return res;
-    } catch (error) {
-      console.error("Error fetching all projects:", error);
-      return [];
-    }
+  if (!id) {
+    return null;
+  }
+  try {
+    const projects = await getProjects(id);
+    return projects || [];
+  } catch (error) {
+    console.error("Error fetching all projects:", error);
+    return [];
+  }
   };
+  
   export const getProjectByDomain = async (domain: string | null) => {
-    if (!domain) {
-      return null;
-    }
-    try {
-      const res = await getDomainProject(domain);
-      return res;
-    } catch (error) {
-      console.error(`Error fetching project for domain ${domain}:`, error);
-      return null;
-    }
+  if (!domain) {
+    return null;
+  }
+  try {
+    const res = await getDomainProject(domain);
+    return res;
+  } catch (error) {
+    console.error(`Error fetching project for domain ${domain}:`, error);
+    return null;
+  }
   };
   
   export const getAnalytics = async (domain: string | null) => {
-    if (!domain) {
-      return null;
-    }
-    try {
-      const res = await getDomainAnalytics(domain);
-      return res;
-    } catch (error) {
-      console.error(`Error fetching analytics for domain ${domain}:`, error);
-      return null;
-    }
+  if (!domain) {
+    return null;
+  }
+  try {
+    const res = await getDomainAnalytics(domain);
+    return res;
+  } catch (error) {
+    console.error(`Error fetching analytics for domain ${domain}:`, error);
+    return null;
+  }
   };
